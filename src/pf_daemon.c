5929caa1745a6db7e586d299bd78bf5353a67f21
Alex Oberhauser <oberhauseralex@networld.to>
2008-06-04 12:57:38 +0200

    Adding an daemon prototype.
    	* src/pf_daemon.c:
    	Making an daemon prototype which controls later one the gsm traffic
    	and reacts than.
    
    	* bin/pf_daemon:
    	Adding the binary file for the daemon (which is compiled for the Intel
    	x86 architecture).

--- src/pf_daemon.c
+++ src/pf_daemon.c
@@ -6,7 +6,16 @@
 
 #define LOCKFILE ".lock"
 
+/**
+ * Starts the program as a daemon and creates a lockfile (specified in the
+ * LOCKFILE constant). So it's impossible to start the daemon twice.
+ */
 void start_daemon();
+
+/**
+ * Stops the daemon and deletes the lockfile, so a new instance can be
+ * started.
+ */
 void stop_daemon();
 
 int main(int argc, char **argv) {
@@ -20,10 +29,6 @@ int main(int argc, char **argv) {
 	stop_daemon();
 }
 
-/**
- * Starts the program as a daemon and creates a lockfile (specified in the
- * LOCKFILE constant). So it's impossible to start the daemon twice.
- */
 void start_daemon() {
 
 	if ( 0 == access(LOCKFILE, 0) ) {
@@ -43,15 +48,11 @@ void start_daemon() {
 		exit(0);
 	} else {
 		FILE *lockfile = fopen(LOCKFILE, "w");
-		fprintf(lockfile, "%d\n\r", getpid());
+		fprintf(lockfile, "%d\n", getpid());
 		fclose(lockfile);
 	}
 }
 
-/**
- * Stops the daemon and deletes the lockfile, so a new instance can be
- * started.
- */
 void stop_daemon() {
 	printf("Daemon will be closed...\n");
 	remove(LOCKFILE);
